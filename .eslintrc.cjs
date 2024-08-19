03861b506cc079c9b5a8a9496e31445709001c61
neptune588 <ctr_nike@naver.com>
2024-08-19 18:23:00 +0900

    [🔨chore]: #3 eslint 밑 prettier 셋팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,22 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
