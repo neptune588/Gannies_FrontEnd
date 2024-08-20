1bb8e7cf0b8fe14bd29a2c0dbb2e8da377479485
VaIice <rlatmdgus499@gmail.com>
2024-08-20 17:05:26 +0900

    [🎨style]: #14 footer 제작 중

---
+++ src/layouts/footer/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Wrapper, Button, ButtonWrapper, DetailsWrapper, Detail, Copyright, Underline } from './style';
+
+function Footer() {
+  return (
+    <Wrapper>
+      <ButtonWrapper>
+        <Button>회사소개</Button>
+        <Button>약관</Button>
+        <Button>개인정보처리방침</Button>
+      </ButtonWrapper>
+      <DetailsWrapper>
+        <Detail>서울특별시 동작구 흑석로 84 중앙대학교 적십자간호대학</Detail>
+        <Detail>교학지원팀: <Underline>02)820-5672</Underline></Detail>
+        <Detail>Fax : <Underline>02-824-7961</Underline></Detail>
+        <Detail>이메일 : <Underline>nursing@cau.ac.kr</Underline></Detail>
+        <Copyright>COPYRIGHT ⓒ 2021 Red Cross College of Nursing, Chung-Ang University. All rights reserved.</Copyright>
+      </DetailsWrapper>
+    </Wrapper>
+  );
+}
+
+export default Footer;
---
+++ src/layouts/footer/style.jsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.footer`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+  background-color: #efefef;
+  height: 208px;
+  width: 1128px;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 1078px;
+  height: 84px;
+  border-bottom: 1px solid black;
+`;
+
+export const Button = styled.button`
+  margin-top: 46px;
+  font-size: ${props => props.theme.typo.size.md};
+  color: ${props => props.theme.colors.primary};
+  cursor: pointer;
+  border: none;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-right: 1px solid #D5D5D5;
+  color: black;
+
+  &:last-of-type {
+    border-right: none;
+  }  
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const DetailsWrapper = styled.div`
+  width: 1078px;
+  margin-top: 34px;
+  margin-bottom: 19px;
+`;
+
+export const Detail = styled.span`
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: 12px;
+  border-right: 1px solid black;
+  color: black;
+
+  &:last-of-type {
+    border-right: none;
+  }  
+`;
+
+export const Underline = styled.span`
+   text-decoration: underline;
+`;
+
+export const Copyright = styled.div`
+  margin-top: 14px;
+  margin-left: 10px;
+  margin-right: 10px;
+  font-size: 12px;
+`;
