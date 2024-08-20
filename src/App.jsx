84a9533a1818ad794e7ad70925c9bd6b24bf8d0f
neptune588 <ctr_nike@naver.com>
2024-08-20 10:56:39 +0900

    [🔨chore]: #3 react-router-dom 셋팅 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import GlobalStyles from '@/styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
+import { RouterProvider } from 'react-router-dom';
 import { theme } from '@/styles/theme';
+import { router } from '@/routes/router';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
