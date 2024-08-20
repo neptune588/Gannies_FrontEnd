94fcc9ed0e70bf1ab83313c3fab2cc4de4e7bc09
neptune588 <ctr_nike@naver.com>
2024-08-20 16:59:07 +0900

    [🎨style]: #9 font 밑 icon 셋팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from '@/App'
 import './index.css'
 import { Provider } from 'react-redux';
-import store from './states/store.js';
+import store from '@/store/store';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
@@ -17,11 +17,9 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
         <QueryClientProvider client={queryClient}>
             <App />
         </QueryClientProvider>
     </Provider>
-  </React.StrictMode>,
 )
