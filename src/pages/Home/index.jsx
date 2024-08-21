7447c818ed1ef7ab3f92e815e0a26b91c0847c75
neptune588 <ctr_nike@naver.com>
2024-08-21 21:32:36 +0900

    [🎨style]: #27 navbar컴포넌트 layout컴포넌트에 적용

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,3 @@
-import Navbar from '@/layouts/Navbar';
 import Banner from '@/pages/Home/Banner';
 import SearchBar from '@/pages/Home/SearchBar';
 import Link from '@/pages/Home/Link';
@@ -7,15 +6,14 @@ import NewPost from '@/pages/Home/Post/NewPost';
 
 function Home() {
   return (
-		<>
-      <Navbar />
+    <>
       <Banner />
       <SearchBar />
       <PopularPost />
       <NewPost />
       <Link />
     </>
-  )
+  );
 }
 
 export default Home;
