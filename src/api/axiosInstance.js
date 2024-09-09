ea998d3bf72ebd787628f2cfe10ea60466c66432
neptune588 <ctr_nike@naver.com>
2024-09-09 23:36:09 +0900

    [🔨chore]: #60 회원 인증(auth)부분 api 호출 함수 정의 완료

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
+import qs from 'qs';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const instance = axios.create({
+export const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 1000,
+  timeout: 5000,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
+});
+
+export const cookieApi = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  withCredentials: true,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
 });
