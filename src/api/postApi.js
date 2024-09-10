9825488a09e06539b27b7d9fe2a01deb5d2880fc
neptune588 <ctr_nike@naver.com>
2024-09-10 23:40:23 +0900

    [🔨chore]: #60 게시글 관련 api 호출 함수 작성 완료

---
+++ src/api/postApi.js
@@ -0,0 +1,36 @@
+import { api, cookieApi } from '@/api/axiosInstance';
+
+//게시물 페이지 단위로 조회 (10개)
+export const getPosts = async (boardType, params) => {
+  const url = `/posts/${boardType}`;
+  const response = await api.get(url, params);
+  return response;
+};
+
+//게시물 생성
+export const createPost = async (boardType, postData) => {
+  const url = `/posts/${boardType}`;
+  const response = await cookieApi.post(url, postData);
+  return response;
+};
+
+//게시물 수정
+export const editPost = async (boardType, postId, postData) => {
+  const url = `/posts/${boardType}/${postId}`;
+  const response = await cookieApi.patch(url, postData);
+  return response;
+};
+
+//게시물 삭제
+export const deletePost = async (boardType, postId) => {
+  const url = `/posts/${boardType}/${postId}`;
+  const response = await cookieApi.delete(url);
+  return response;
+};
+
+//게시물 신고
+export const postReport = async (boardType, postId, reportData) => {
+  const url = `/posts/${boardType}/${postId}/reports`;
+  const response = await cookieApi.post(url, reportData);
+  return response;
+};
