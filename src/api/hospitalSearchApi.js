cbcdb70592fa997393eb124936376d2d8dda83c8
neptune588 <ctr_nike@naver.com>
2024-09-11 20:09:15 +0900

    [Ⓜ️rename] : #60 환경 변수 이름 수정

--- src/api/hospitalSearchApi.js
+++ src/api/hospitalSearchApi.js
@@ -3,6 +3,6 @@ import { hospitalApi } from '@/api/axiosInstance';
 //병원 찾기
 export const getHospitals = async (params) => {
   const url = '';
-  const response = await hospitalApi.post(url, params);
+  const response = await hospitalApi.get(url, params);
   return response;
 };
