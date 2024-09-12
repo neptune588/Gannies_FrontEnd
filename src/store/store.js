4c95b2809e2a456317a78ee05c7170b500ed0cde
neptune588 <ctr_nike@naver.com>
2024-09-12 18:41:45 +0900

    [✨feat]: #64 커뮤니티 페이지(게시물 모록) slice 작성 완료.

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage/session';
+
 import num from './num-slice';
 import navBarOptionsReducer from '@/store/navBarOptions';
+import communityDataReducer from '@/store/communityData';
 
 const navBarOptionsPersistConfig = {
   key: 'navBarOptions', // 저장소에서 상태를 식별할 키
@@ -18,6 +20,7 @@ const store = configureStore({
   reducer: {
     num: num,
     navBarOptions: persistedNavBarOptionsReducer,
+    communityData: communityDataReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
