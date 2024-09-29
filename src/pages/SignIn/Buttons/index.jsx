d3cc405ac73c35cbe8643c6095d23a3f102a0f15
neptune588 <ctr_nike@naver.com>
2024-09-29 21:33:11 +0900

    [🎨style]: #70 로딩 컴포넌트 추가

--- src/pages/SignIn/Buttons/index.jsx
+++ src/pages/SignIn/Buttons/index.jsx
@@ -13,7 +13,7 @@ import {
 import { setLogin } from '@/store/auth';
 import { userSignIn } from '@/api/authApi';
 
-function Buttons({ email, password, setLoginError }) {
+function Buttons({ email, password, setLoginError, setIsLoading }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -25,6 +25,7 @@ function Buttons({ email, password, setLoginError }) {
 
   const handleLogin = async () => {
     try {
+      setIsLoading(true);
       const response = await userSignIn({ email: email, password: password });
 
       const { userId } = response.data;
@@ -37,20 +38,22 @@ function Buttons({ email, password, setLoginError }) {
   };
 
   return (
-    <Wrapper>
-      <div>
-        <button onClick={handleAutoLogin}>
-          <input type='checkbox' checked={autoLogin} readOnly />
-          <span>자동로그인</span>
-        </button>
-        <FindButton to='/find/id'>아이디 / 비밀번호 찾기</FindButton>
-      </div>
-      <NextButton $margin='47px' text='로그인' onClick={handleLogin} />
-      <span>
-        아직 회원이 아니신가요?&nbsp;&nbsp;
-        <SignUpButton to='/sign-up/identity'>회원가입</SignUpButton>
-      </span>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <div>
+          <button onClick={handleAutoLogin}>
+            <input type='checkbox' checked={autoLogin} readOnly />
+            <span>자동로그인</span>
+          </button>
+          <FindButton to='/find/id'>아이디 / 비밀번호 찾기</FindButton>
+        </div>
+        <NextButton $margin='47px' text='로그인' onClick={handleLogin} />
+        <span>
+          아직 회원이 아니신가요?&nbsp;&nbsp;
+          <SignUpButton to='/sign-up/identity'>회원가입</SignUpButton>
+        </span>
+      </Wrapper>
+    </>
   );
 }
 
